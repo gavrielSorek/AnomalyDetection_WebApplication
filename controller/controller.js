288c332f7a5070226503398c5794f5d7a90cf5e7
Roy <royy1230@gmail.com>
2021-05-04 18:29:25 +0300

    add new http requests + create model item on the model

--- controller/controller.js
+++ controller/controller.js
@@ -4,6 +4,8 @@ var app = express();
 var fs = require("fs");
 const model = require('../model/model')
 
+
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
@@ -11,11 +13,86 @@ app.use(bodyParser.json())
 
 app.post('/api/model', function (req, res, next) { //next requrie (the function will not stop the program)
    const type = req.query.model_type; //type = hybrid/regression
-   //console.log(type);
+ //  console.log("the type is :" + type);
    const data = req.body; //data is the object that the json body contain
-   model.writeTrain(req, res, data)
+   var modelItem = model.writeTrain(req, res, data)
+   var json_res = {
+      model_id: modelItem.id,
+      upload_time: modelItem.datetime,
+      status: "ready"
+   }
+   res.json(json_res);
+
    next();
 })
+
+app.get('/api/model' , function (req, res ,next) {
+      const id  = req.query.model_id;
+      var m = model.isMoudoleExsist(id);
+     // console.log("the id is " + id);
+     // console.log("the item is :" + m)
+      if(m){
+      var json_res = {
+         model_id: m.id,
+         upload_time: m.datetime,
+         status: "ready"
+      }
+   
+      res.json(json_res);
+      res.status(200);
+   }
+   else{
+      res.status(400);
+   }
+      next();
+})
+
+app.delete('/api/model' , function (req, res ,next) {
+   const id  = req.query.model_id;
+   var m = model.isMoudoleExsist(id);
+   if(m){
+      model.deleteModel(id)
+      res.status(200);
+      console.log("item deleted sucssfully");
+   }else{
+      console.log("item not found, error 400");
+      res.status(400);
+   }
+   next();
+})
+
+app.get('/api/models' , function (req, res ,next) {
+   var json={};
+   var modelArr = new Array();
+   models = model.getModels();
+   models.forEach((values,keys)=>{
+      jsonItem= {
+         model_id: values.id,
+         upload_time: values.datetime,
+         status: "ready"
+      } 
+      modelArr.push(jsonItem);
+   })
+
+ //console.log(json);
+ res.json(modelArr);
+   res.status(200);
+   next();
+})
+
+app.post('/api/anomaly' , function (req, res ,next) {
+   const id  = req.query.model_id;
+   const data_to_detect = req.body; //data is the object that the json body contain
+   if(!model.isMoudoleExsist(id)){
+      console.status(400);
+      console.log("model does not exsist");
+   }
+   else{
+      model.createAnnomalyFile(id,data_to_detect);
+      // TODO add annomaly
+   }
+   next();
+}) 
 
 var server = app.listen(9876, function () {
    var host = server.address().address
--- model/model.js
+++ model/model.js
@@ -1,4 +1,4 @@
-//imports
+// const modelItem = require('./modelItem.js')
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -8,15 +8,26 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
  var id = 1; //global vatiable to represent all the id
+ let modelMap = new Map();
+
+ function moddelItem(id,type,datetime,status,fileName){
+           this.id = id;
+           this.type = type;
+           this.datetime = datetime;
+           this.status = status;
+           this.fileName = fileName;
+           this.annomalyFile = null;
+   }
 
 function writeTrain(req, res, data) {
     let csvHeader = []
     for (const property in data) {
        csvHeader.push({ id: property, title: property });
     }
- 
+    var path = '../model/train' + (id++) +'.csv'
+
     const csvWriter = createCsvWriter({
-       path: '../model/train' + (id++) +'.csv',
+       path: path,
        header: csvHeader
     });
  
@@ -30,7 +41,57 @@ function writeTrain(req, res, data) {
        attrObjArry.push(singleObj);
     }
     csvWriter.writeRecords(attrObjArry);
+    let modelItem = new moddelItem(id,req.query.model_type,new Date(),"ready",path);
+    modelMap.set(modelItem.id,modelItem);
+ // let moddelItem = new moddelItem();
+    return modelItem;
 }
+
+function createAnnomalyFile(itemID,data){
+   let csvHeader = []
+   for (const property in data) {
+      csvHeader.push({ id: property, title: property });
+   }
+   var path = '../model/anommaly' + (itemID) +'.csv'
+
+   const csvWriter = createCsvWriter({
+      path: path,
+      header: csvHeader
+   });
+
+   var i;
+   var attrObjArry = [];
+   for (i = 0; i < data[csvHeader[0].id].length; i++) { //create array of object that contain the info of data accordingly to the titles
+      var singleObj = {};
+      for (const property in data) {
+         singleObj[property] = data[property][i];
+      }
+      attrObjArry.push(singleObj);
+   }
+   csvWriter.writeRecords(attrObjArry);
+
+   modelItem = modelMap.get(parseInt(itemID));
+   modelItem.annomalyFile = path;
+
+}
+
+function isMoudoleExsist(itemID){
+   if(modelMap.has(parseInt(itemID))){
+      return modelMap.get(parseInt(itemID));
+   }
+   else{
+      return null;
+   }
+}
+
+function deleteModel(itemID){
+   modelMap.delete(parseInt(itemID));
+}
+
+function getModels(){
+   return modelMap;
+}
+
 module.exports = {
-    writeTrain
+    writeTrain,isMoudoleExsist,deleteModel,getModels,createAnnomalyFile
 };
