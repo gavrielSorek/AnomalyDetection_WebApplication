0288bc2bc329d1210ccf4462e4c94df5080f6776
Gavriel Sorek <gavrielsorek@gmail.com>
2021-05-26 17:49:12 +0300

    update double columns

--- model/Test.js
+++ model/Test.js
@@ -1,7 +1,10 @@
-const cppFile = require('./AnomalyDetector.node');
-var detector = new cppFile.SimpleAnomalyDetectorJS("132");
+const cppFile = require('../AnomalyDetectorByOs/windows/AnomalyDetector.node');
+var detector = new cppFile.HybridAnomalyDetectorJS("132");
 //detector.LearnNormal("train1.csv",AsyncWorkerCompletion);
-detector.LearnNormal("..\\testsCsv\\reg_flight.csv",AsyncWorkerCompletion);
+//detector.LearnNormal("..\\testsCsv\\reg_flight.csv",AsyncWorkerCompletion);
+detector.LearnNormal("train1",AsyncWorkerCompletion);
+
+//detector.LearnNormal("..\\testsCsv\\reg_flight.csv",AsyncWorkerCompletion);
 //console.log("runSimpleAsyncWorker returned '"+result+"'.");
 
 var m = {};
@@ -13,8 +16,9 @@ function AsyncWorkerCompletion (err, result) {
         console.log("anomaly detector returned an error: ", err);
     } else {
         console.log("anomaly detector returned '"+result+"'.");
+        console.log("anomaly detector returned wwwwwwwwwwwwwwwwwwwwwwww '"+result+"'.");
         //console.log(detector);
-        var res = detector.DetectAnomalies("..\\testsCsv\\anomaly_flight.csv");
+        var res = detector.DetectAnomalies("train1");
         console.log(res);
             }
     
